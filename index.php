90aa9e11921254c31c321e1ee4095cc507e50ae4
ysabel <ysabelliu276@gmail.com>
2018-01-15 04:47:13 +0800

    Added Student Class

--- index.php
+++ index.php
@@ -12,7 +12,37 @@ and open the template in the editor.
     <body>
         <?php
         // put your code here
-        echo "Hello out there ... I live";
+        include('Student.php');
+
+        $students = array();
+        
+        $first = new Student();
+        $first->surname = "Doe";
+        $first->first_name = "John";
+        $first->add_email('home','john@doe.com');
+        $first->add_email('work','jdoe@mcdonalds.com');
+        $first->add_grade(65);
+        $first->add_grade(75);
+        $first->add_grade(55);
+        $students['j123'] = $first;
+        
+        $second = new Student();
+        $second->surname = "Einstein";
+        $second->first_name = "Albert";
+        $second->add_email('home','albert@braniacs.com');
+        $second->add_email('work1','a_einstein@bcit.ca');
+        $second->add_email('work2','albert@physics.mit.edu');
+        $second->add_grade(95);
+        $second->add_grade(80);
+        $second->add_grade(50);
+        $students['a456'] = $second;
+        
+        ksort($students);
+
+        foreach($students as $student)
+            echo $student->toString();
+        
+        
         ?>
     </body>
 </html>
